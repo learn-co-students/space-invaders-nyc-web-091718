8a8e56b832b8a8acdadf8e0eed8f265ba885fb44
kristopherbullinger <github email address>
2018-11-06 17:22:40 -0500

    Lab Complete

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,29 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position
+    this.currentShip = "Looking for a rig"
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip === "Looking for a rig") {
+      return "had no effect"
+    } else if (this.position === "Pilot") {
+      this.currentShip.warpDrive = "Engaged"
+    }
+  }
+  setsInvisibility() {
+    if (this.currentShip === "Looking for a rig") {
+      return "had no effect"
+    } else if (this.position === "Defender"){
+      this.currentShip.cloaked = true
+    }
+  }
+  chargePhasers() {
+    if (this.currentShip === "Looking for a rig") {
+      return "had no effect"
+    } else if (this.position === "Gunner") {
+      this.currentShip.phasersCharge = "charged"
+    }
+  }
+
+}
