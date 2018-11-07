452e1c4c79ece7ac5c1fb5ceb048800d1838f28c
Z1mm3r <czimmer@ramapo.edu>
2018-11-07 15:57:40 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,45 @@
-class CrewMember {}
+class CrewMember {
+
+  constructor(position){
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+    this.chargePhasers = function(){return'had no effect'}
+    this.setsInvisibility = function(){return'had no effect'}
+    this.engageWarpDrive = function(){return'had no effect'}
+
+    switch(position){
+
+      case 'Gunner':
+        this.chargePhasers = function (){
+          if(this.currentShip == 'Looking for a Rig'){
+            return 'had no effect'
+          }
+          else {
+            this.currentShip.phasersCharge = 'charged'
+          }
+        }
+        break;
+
+      case 'Defender':
+        this.setsInvisibility = function (){
+          if(this.currentShip == 'Looking for a Rig'){
+            return 'had no effect'
+          }
+          else {
+            this.currentShip.cloaked = true
+          }
+        }
+        break;
+
+      case 'Pilot':
+        this.engageWarpDrive = function (){
+          if(this.currentShip == 'Looking for a Rig'){
+            return 'had no effect'
+          }
+        }
+        break;
+
+    }
+  }
+
+}
