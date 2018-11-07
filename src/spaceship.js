c3c59ae29bf53c2c03373f2ba99a9d76b1c5ab29
Brennan Blair <bblair31@users.noreply.github.com>
2018-11-07 08:56:32 -0500

    Done

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,26 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, numPhasers, numShields){
+    this.name = name
+    this.crew = crew
+    this.phasers = numPhasers
+    this.shields = numShields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+    this.docked = (() => {
+      if (crew.length) {
+        return false
+      } else {
+        return true
+      }
+    })()
+    this.populateCrew()
+  }
+
+  populateCrew(){
+    this.crew.forEach((crewMember) => {
+      return crewMember.currentShip = this
+    })
+  }
+
+} // End of Spaceship Class
