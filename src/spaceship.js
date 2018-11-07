2243409e77425c4253769a7950140802aaab053d
AsafDavidov <adavidov@conncoll.edu>
2018-11-07 10:39:31 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,13 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name,crew = [],phasers,shields){
+    this.name = name;
+    this.crew = crew;
+    crew.forEach((crew)=>crew.currentShip = this);
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.docked = (this.crew.length === 0) ? true:false;
+    this.phasersCharge = "uncharged";
+  }
+}
